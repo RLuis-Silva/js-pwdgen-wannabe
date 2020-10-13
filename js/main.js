d37eb48675ced5cc1f230088900f6adab8d9ba7e
Luis Silva <s.ros.luis@gmail.com>
2020-10-13 15:48:23 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("javascript funziona");
+// console.log("javascript funziona");
+
+
+var nomeUtente = prompt("Inserire il tuo nome");
+// console.log("nome utente: ", nomeUtente);
+
+
+var cognomeUtente = prompt("Inserire il tuo cognome");
+// console.log("cognome utente: ", cognomeUtente);
+
+var colorePrefUtente = prompt("inserire il tuo colore preferito");
+// console.log("colore preferito dell' utente: ", colorePrefUtente);
+
+var passwordRandom = nomeUtente + cognomeUtente + colorePrefUtente + "19";
+// console.log("password generata: ", passwordRandom);
+
+// output
+document.getElementById('nome-utente').innerHTML = nomeUtente;
+document.getElementById('cognome-utente').innerHTML = cognomeUtente;
+document.getElementById('password').innerHTML = passwordRandom;
